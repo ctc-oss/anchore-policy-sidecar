9b063b1a3b2e6e765a9325adbfc8d3e6661ec8fa
John Wass <wassj@ctc.com>
2020-09-26 08:39:46 -0400

    round trip with auth

--- src/main/scala/com/ctc/g2w/boot.scala
+++ src/main/scala/com/ctc/g2w/boot.scala
@@ -1,5 +1,7 @@
 package com.ctc.g2w
 
+import com.ctc.g2w.anchore.OAuthResponse
+import spray.json._
 import zio._
 import zio.config.{ReadError, ZConfig, getConfig}
 import zio.console._
@@ -10,8 +12,12 @@ object boot extends scala.App {
 
   val app = for {
     c <- getConfig[anchore.config.Http]
-    r <- ZIO.effect(requests.get(s"${c.url()}/health").statusCode).catchAll(_ => IO.succeed(999))
-    _ <- putStrLn(s"result: $r")
+    hc <- ZIO.effect(requests.get(s"${c.url()}/health").statusCode).catchAll(_ => IO.succeed(999))
+    _ <- putStrLn(s"result: $hc")
+    auth <- ZIO.effect(requests.post(s"${c.url()}/oauth/token", data = anchore.defaultAuth).text().parseJson.convertTo[OAuthResponse])
+    _ <- putStrLn(s"result: $auth")
+    p <- ZIO.effect(requests.get(url = s"${c.url()}/policies", headers = auth.headers))
+    _ <- putStrLn(s"result: $p")
   } yield ()
 
   Runtime.unsafeFromLayer(cfg ++ Console.live).unsafeRun(app)
