9be71808aa037beece16fdfa63fa9b812f5bff3e
John Wass <wassj@ctc.com>
2020-09-26 15:43:34 -0400

    sketch in auth and api environments

--- src/main/scala/com/ctc/g2w/anchore.scala
+++ src/main/scala/com/ctc/g2w/anchore.scala
@@ -1,10 +1,66 @@
 package com.ctc.g2w
 
+import java.time.Instant
+
 import requests.RequestBlob
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+import zio._
 
 object anchore {
   type HttpConfig = zio.config.ZConfig[anchore.config.Http]
+
+  type AnchoreAPI = Has[AnchoreAPI.Service]
+  object AnchoreAPI {
+    trait Service {
+      def health(): UIO[anchore.api.Health]
+    }
+
+    def live(cfg: anchore.config.Http): Layer[Has[AnchoreAuth], AnchoreAPI] = ZLayer.succeed(
+      new AnchoreAPI.Service {
+        def health(): UIO[api.Health] =
+          ZIO
+            .effect(requests.get(s"${cfg.url()}/health").statusCode)
+            .map {
+              case 200 => anchore.api.Health.Ok
+            }
+            .orElse(ZIO.succeed(anchore.api.Health.Fail))
+      }
+    )
+
+    def health(): URIO[AnchoreAPI, anchore.api.Health] = ZIO.accessM(_.get.health())
+  }
+
+  type AnchoreAuth = Has[AnchoreAuth.Service]
+  object AnchoreAuth {
+    trait Service {
+      def token(): Task[OAuthToken]
+    }
+
+    def make(cfg: anchore.config.Http): Layer[Nothing, AnchoreAuth] = ZLayer.fromEffect(
+      Ref.make(OAuthToken.Expired).map { tokenRef =>
+        new AnchoreAuth.Service {
+          import spray.json._
+          val tokenBuffer = 100 // todo;; move to config
+          def token(): Task[OAuthToken] =
+            tokenRef.get.flatMap { tok =>
+              if (tok.expiresWithin(tokenBuffer))
+                ZIO
+                  .effect(
+                    requests
+                      .post(s"${cfg.url()}/oauth/token", data = anchore.defaultAuth)
+                      .text()
+                      .parseJson
+                      .convertTo[OAuthResponse]
+                  )
+                  .map(r => OAuthToken(r.access_token, Instant.now().plusSeconds(r.expires_in)))
+              else Task.succeed(tok)
+            }
+        }
+      }
+    )
+
+    def token(): RIO[AnchoreAuth, OAuthToken] = ZIO.accessM(_.get.token())
+  }
 
   // grant_type=password&client_id=anonymous&username=admin&password=foobar
   val defaultAuth = OAuthCredentials("anonymous", "admin", "foobar")
@@ -16,6 +72,13 @@ object anchore {
     )
   }
 
+  case class OAuthToken(value: String, expires: Instant) {
+    def expiresWithin(sec: Int): Boolean = expires.plusSeconds(sec).isAfter(expires)
+    def isExpired: Boolean = Instant.now().isAfter(expires)
+  }
+  object OAuthToken {
+    val Expired = OAuthToken("_", Instant.ofEpochSecond(0))
+  }
   case class OAuthResponse(access_token: String, expires_in: Int)
   object OAuthResponse extends DefaultJsonProtocol {
     implicit val format: RootJsonFormat[OAuthResponse] = jsonFormat2(OAuthResponse.apply)
@@ -40,6 +103,15 @@ object anchore {
   }
 
   object api {
+    sealed trait Health
+    object Health {
+      object Ok extends Health {
+        override def toString: String = "Ok"
+      }
+      object Fail extends Health {
+        override def toString: String = "Fail"
+      }
+    }
     case class PolicyBundle(
         id: String,
         name: Option[String],
--- src/main/scala/com/ctc/g2w/boot.scala
+++ src/main/scala/com/ctc/g2w/boot.scala
@@ -1,24 +1,28 @@
 package com.ctc.g2w
 
-import com.ctc.g2w.anchore.OAuthResponse
-import spray.json._
+import com.ctc.g2w.anchore.{AnchoreAPI, AnchoreAuth}
 import zio._
 import zio.config.{ReadError, ZConfig, getConfig}
 import zio.console._
 import zio.system.System
 
 object boot extends scala.App {
+  val HackConfigForNow = anchore.config.Http("localhost", 8228)
+
   val cfg: Layer[ReadError[String], anchore.HttpConfig] = System.live >>> ZConfig.fromSystemEnv(anchore.config.http)
+  val api = AnchoreAPI.live(HackConfigForNow)
+  val auth = AnchoreAuth.make(HackConfigForNow)
+  val deps = cfg ++ auth ++ api ++ Console.live
 
   val app = for {
     c <- getConfig[anchore.config.Http]
-    hc <- ZIO.effect(requests.get(s"${c.url()}/health").statusCode).catchAll(_ => IO.succeed(999))
+    hc <- AnchoreAPI.health()
     _ <- putStrLn(s"result: $hc")
-    auth <- ZIO.effect(requests.post(s"${c.url()}/oauth/token", data = anchore.defaultAuth).text().parseJson.convertTo[OAuthResponse])
-    _ <- putStrLn(s"result: $auth")
-    p <- ZIO.effect(requests.get(url = s"${c.url()}/policies", headers = auth.headers))
+    token <- AnchoreAuth.token()
+    _ <- putStrLn(s"result: $token")
+    p <- ZIO.effect(requests.get(url = s"${c.url()}/policies", headers = Map("Authorization"->s"Bearer ${token.value}")))
     _ <- putStrLn(s"result: $p")
   } yield ()
 
-  Runtime.unsafeFromLayer(cfg ++ Console.live).unsafeRun(app)
+  Runtime.unsafeFromLayer(deps).unsafeRun(app)
 }
